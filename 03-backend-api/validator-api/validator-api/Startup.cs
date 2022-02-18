9116e871673da51a7436bd0e950bae465d78311e
darren <darren.ly.ong@kerrylogistics.com>
2022-02-18 21:17:05 +0800

    feat: added swagger

--- 03-backend-api/validator-api/validator-api/Startup.cs
+++ 03-backend-api/validator-api/validator-api/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace validator_api
 {
@@ -27,6 +28,20 @@ namespace validator_api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Validator API",
+                    Version = "v1",
+                    Description = "Validator",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Darren Ong",
+                        Email = string.Empty,
+                    },
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
@@ -46,10 +61,18 @@ namespace validator_api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Welcome to running ASP.NET Core on AWS Lambda");
-                });
+            });
+
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Validator API V1");
+
+                // To serve SwaggerUI at application's root page, set the RoutePrefix property to an empty string.
+                c.RoutePrefix = String.Empty;
             });
         }
     }
