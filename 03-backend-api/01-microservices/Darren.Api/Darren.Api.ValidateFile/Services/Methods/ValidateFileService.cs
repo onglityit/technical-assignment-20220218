5887c023b45322f41a6cba612b406c7c93ad6c1e
darren <darren.ly.ong@kerrylogistics.com>
2022-02-18 23:00:07 +0800

    feat: add test for file extensions

--- 03-backend-api/01-microservices/Darren.Api/Darren.Api.ValidateFile/Services/Methods/ValidateFileService.cs
+++ 03-backend-api/01-microservices/Darren.Api/Darren.Api.ValidateFile/Services/Methods/ValidateFileService.cs
@@ -1,13 +1,16 @@
 ﻿using Darren.Api.ValidateFile.Services.Interface;
 using System;
+using System.Linq;
 
 namespace Darren.Api.ValidateFile.Services.Methods
 {
     public class ValidateFileService : IValidateFileService
     {
+        private readonly String[] SUPPORT_FORMAT = { "XML", "CSV" }; 
         public bool CheckFileExtension(String extension)
         {
-            return false;
+            if(!SUPPORT_FORMAT.Contains(extension.ToUpper())) return false;
+            return true;
         }
         public bool CheckFileSize(int fileSizeBytes)
         {
