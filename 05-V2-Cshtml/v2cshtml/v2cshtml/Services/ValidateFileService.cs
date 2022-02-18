43a546a6be4acd63761e9fe502856ae0a23ffafc
darren <darren.ly.ong@kerrylogistics.com>
2022-02-19 01:47:26 +0800

    feat: val file size 1MB max

--- 05-V2-Cshtml/v2cshtml/v2cshtml/Services/ValidateFileService.cs
+++ 05-V2-Cshtml/v2cshtml/v2cshtml/Services/ValidateFileService.cs
@@ -5,9 +5,16 @@ namespace v2cshtml.Services
     public class ValidateFileService
     {
         public IFormFile file1 { get; set; }
+        private readonly int MAX_FILE_SIZE = 1024 * 1024;
         private readonly String[] SUPPORTED_EXTENSIONS = { "CSV", "XML" };
 
         public ValidateFileResponseModel ValidateFile()
+        {
+            ValidateFileResponseModel vfrm = ValidateFileExt();
+            vfrm = ValidateSize(vfrm);
+            return vfrm;
+        }
+        public ValidateFileResponseModel ValidateFileExt()
         {
             ValidateFileResponseModel vfrm = new ValidateFileResponseModel()
             {
@@ -23,6 +30,21 @@ namespace v2cshtml.Services
             {
                 vfrm.Success = false;
                 vfrm.ErrorMessage += "Unknown format\r\n";
+            }
+            return vfrm;
+        }
+
+        public ValidateFileResponseModel ValidateSize(ValidateFileResponseModel vfrm)
+        {
+            if (file1 == null || file1.Length == 0)
+            {
+                vfrm.Success = false;
+                vfrm.ErrorMessage += "File is empty!\r\n";
+            }
+            else if (file1.Length > MAX_FILE_SIZE)
+            {
+                vfrm.Success=false;
+                vfrm.ErrorMessage += "Max file size is 1MB\r\n";
             }
             return vfrm;
         }
