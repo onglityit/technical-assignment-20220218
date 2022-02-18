06cb2d604e41af96f4fd5f37d9553eab028214a6
darren <darren.ly.ong@kerrylogistics.com>
2022-02-19 06:51:43 +0800

    feat: add blob storage library

--- 05-V2-Cshtml/v2cshtml/v2cshtml/Program.cs
+++ 05-V2-Cshtml/v2cshtml/v2cshtml/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.Extensions.Azure;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddAzureClients(clientBuilder =>
+{
+    clientBuilder.AddBlobServiceClient(builder.Configuration["darren-azurite:blob"], preferMsi: true);
+    clientBuilder.AddQueueServiceClient(builder.Configuration["darren-azurite:queue"], preferMsi: true);
+});
 
 var app = builder.Build();
 
