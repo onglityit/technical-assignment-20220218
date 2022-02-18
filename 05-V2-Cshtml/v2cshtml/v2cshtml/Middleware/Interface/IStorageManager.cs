3418217cef728123f99a8e9739627292f13c3505
darren <darren.ly.ong@kerrylogistics.com>
2022-02-19 07:20:43 +0800

    feat: add azure blob storage library

---
+++ 05-V2-Cshtml/v2cshtml/v2cshtml/Middleware/Interface/IStorageManager.cs
@@ -0,0 +1,7 @@
+﻿namespace v2cshtml.Middleware.Interface
+{
+    public interface IStorageManager
+    {
+        Task<string> WriteTo(string fileName, string content);
+    }
+}
---
+++ 05-V2-Cshtml/v2cshtml/v2cshtml/Middleware/Method/StorageManager.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using v2cshtml.Middleware.Interface;
+
+namespace v2cshtml.Middleware.Method
+{
+    public class StorageManager : IStorageManager
+    {
+        private readonly IConfiguration config;
+        private CloudStorageAccount account;
+        private CloudBlobClient cloudBlobClient;
+        private CloudBlobContainer container;
+        public StorageManager(IConfiguration _config)
+        {
+            config = _config;
+            string blobStorageConnString = _config["AzureBlobStorage:ConnectionString"].ToString();
+            string containerName = _config["AzureBlobStorage:ContainerName"].ToString();
+            if (CloudStorageAccount.TryParse(blobStorageConnString, out account))
+            {
+                cloudBlobClient = account.CreateCloudBlobClient();
+                container = cloudBlobClient.GetContainerReference(containerName);
+            }
+        }
+
+        Task<string> IStorageManager.WriteTo(string fileName, string content)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
