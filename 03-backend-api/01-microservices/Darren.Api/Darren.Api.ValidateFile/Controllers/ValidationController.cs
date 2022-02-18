cc38b4870db9a65e3c9af6715d37eba0858fd249
darren <darren.ly.ong@kerrylogistics.com>
2022-02-18 22:00:35 +0800

    feat: clean project name

---
+++ 03-backend-api/01-microservices/Darren.Api/Darren.Api.ValidateFile/Controllers/ValidationController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Darren.Api.ValidateFile.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ValidationController : ControllerBase
+    {
+        [HttpPost("validateFile")]
+        public async Task<ActionResult> ValidateFile([FromForm] IFormFile UploadedFile)
+        {
+
+            return Ok();
+        }
+    }
+}
