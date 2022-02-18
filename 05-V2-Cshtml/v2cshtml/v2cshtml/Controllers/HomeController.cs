9cc04e85fcb54921b172f78939fa946c327ef1e2
darren <darren.ly.ong@kerrylogistics.com>
2022-02-19 01:01:32 +0800

    feat: cshtml project

---
+++ 05-V2-Cshtml/v2cshtml/v2cshtml/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using v2cshtml.Models;
+
+namespace v2cshtml.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost("ValidateFile")]
+        public async Task<IActionResult> ValidateFile()
+        {
+            return Ok("test");
+        }
+    }
+}
---
+++ 05-V2-Cshtml/v2cshtml/v2cshtml/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace v2cshtml.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
