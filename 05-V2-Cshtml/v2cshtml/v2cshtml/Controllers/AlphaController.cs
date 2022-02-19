d5b184bba766c110b141cd815ea0791b75147410
darren <darren.ly.ong@kerrylogistics.com>
2022-02-19 11:41:11 +0800

    feat: tested alpha hardcode file path

---
+++ 05-V2-Cshtml/v2cshtml/v2cshtml/Controllers/AlphaController.cs
@@ -0,0 +1,30 @@
+﻿using CsvHelper;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using v2cshtml.Models.alpha;
+
+namespace v2cshtml.Controllers
+{
+    public class AlphaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("Alpha")]
+        public async Task<IActionResult> Alpha(IFormFile file1)
+        {
+            using (var reader = new StreamReader("c:\\pathcsv\\file.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<CsvIdName>().ToList();
+                foreach(CsvIdName rec in records)
+                {
+                    
+                }
+            }
+            return Ok();        
+        }
+    }
+}
---
+++ 05-V2-Cshtml/v2cshtml/v2cshtml/Models/alpha/CsvIdName.cs
@@ -0,0 +1,8 @@
+﻿namespace v2cshtml.Models.alpha
+{
+    public class CsvIdName
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
