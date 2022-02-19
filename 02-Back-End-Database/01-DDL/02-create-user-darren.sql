7e46bbd42d62b71cce0004cd812f06bf9adb5083
darren <darren.ly.ong@kerrylogistics.com>
2022-02-19 18:21:06 +0800

    fix: to allow for mix authentication

--- 02-Back-End-Database/01-DDL/02-create-user-darren.sql
+++ 02-Back-End-Database/01-DDL/02-create-user-darren.sql
@@ -14,3 +14,10 @@ USE [InternationalTransaction]
 GO
 CREATE SCHEMA [darren] AUTHORIZATION [darren]
 GO
+
+
+
+USE [master]
+GO
+EXEC xp_instance_regwrite N'HKEY_LOCAL_MACHINE', N'Software\Microsoft\MSSQLServer\MSSQLServer', N'LoginMode', REG_DWORD, 2
+GO
