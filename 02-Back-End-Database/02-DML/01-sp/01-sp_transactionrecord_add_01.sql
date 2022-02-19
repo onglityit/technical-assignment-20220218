e7b5b50df799a2d84334cb8126de70d770bdc02d
darren <darren.ly.ong@kerrylogistics.com>
2022-02-19 19:04:16 +0800

    feat: wip call stored proc

--- 02-Back-End-Database/02-DML/01-sp/01-sp_transactionrecord_add_01.sql
+++ 02-Back-End-Database/02-DML/01-sp/01-sp_transactionrecord_add_01.sql
@@ -34,6 +34,46 @@ BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
+	DECLARE @ret_is_success INT = 0;
+	DECLARE @ret_err_msg VARCHAR(50) = '';
+
+	IF NOT EXISTS(
+	SELECT 1 FROM [dbo].[transactionrecord] WHERE 
+	@fileguid = fileguid AND linenumber = @linenumber
+	)
+	BEGIN
+		INSERT INTO [dbo].[transactionrecord] (
+			fileguid		,
+			linenumber		,
+			bloburi			,
+			transactionid	,
+			amount			,
+			currencycode	,
+			transactiondate ,
+			statuscode		
+		)
+		SELECT 
+			@fileguid		 fileguid		  ,
+			@linenumber		 linenumber		  ,
+			@bloburi		 bloburi		  ,
+			@transactionid	 transactionid	  ,
+			@amount			 amount			  ,
+			@currencycode	 currencycode	  ,
+			@transactiondate transactiondate  ,
+			@statuscode		 statuscode		
+	END
+	ELSE BEGIN
+
+		SET @ret_is_success = 1;
+		IF @istest = 1 
+		BEGIN
+			SET @ret_err_msg = 'Skipped Insert due to test';
+		END
+		ELSE BEGIN
+			SET @ret_err_msg = 'Skipped Insert due to duplicate';
+		END
+
+	END 
 
     -- Insert statements for procedure here
 	SELECT 1 [is_success]
--- 05-V2-Cshtml/v2cshtml/FunctionCsvXmlBlobTrigger/Services/DbWriteRecordServices.cs
+++ 05-V2-Cshtml/v2cshtml/FunctionCsvXmlBlobTrigger/Services/DbWriteRecordServices.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -20,17 +21,46 @@ namespace FunctionCsvXmlBlobTrigger.Services
             int linenumber ,
             string bloburi ,
             string transactionid ,
-            string amount ,
+            decimal amount ,
             string currencycode ,
-            string transactiondate ,
+            DateTime transactiondate ,
             string statuscode ,
             bool istest = false
             )
         {
             string ret = string.Empty;
-            using (var conn = new SqlConnection())
+            using (var conn = new SqlConnection(config["SqlConnectionString"]))
             {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = "sp_transactionrecord_add_01";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Connection = conn;
 
+                    if (cmd.Connection.State != ConnectionState.Open) cmd.Connection.Open();
+                    cmd.Parameters.Add(new SqlParameter("@fileguid", SqlDbType.NVarChar) { Value = fileguid });
+                    cmd.Parameters.Add(new SqlParameter("@linenumber", SqlDbType.Int) { Value = linenumber });
+                    cmd.Parameters.Add(new SqlParameter("@bloburi", SqlDbType.NVarChar) { Value = bloburi });
+                    cmd.Parameters.Add(new SqlParameter("@transactionid", SqlDbType.NVarChar) { Value = transactionid });
+                    cmd.Parameters.Add(new SqlParameter("@amount", SqlDbType.Decimal) { Value = amount });
+                    cmd.Parameters.Add(new SqlParameter("@currencycode", SqlDbType.NChar) { Value = currencycode });
+                    cmd.Parameters.Add(new SqlParameter("@transactiondate", SqlDbType.DateTime2) { Value = transactiondate });
+                    cmd.Parameters.Add(new SqlParameter("@PASSWORD", SqlDbType.NVarChar) { Value = statuscode });
+                    cmd.Parameters.Add(new SqlParameter("@PASSWORD", SqlDbType.Int) { Value = 0 });
+
+                    SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+                    if (reader.Read())
+                    {
+                        string isSuccess = reader["is_success"].ToString();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    log.LogInformation(ex.Message);
+
+                }
             }
             return ret;
         }
