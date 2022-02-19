1af5311f902b2c7e6338cdf42512d15b83faa588
darren <darren.ly.ong@kerrylogistics.com>
2022-02-19 17:49:46 +0800

    feat: use binding for uri

--- 05-V2-Cshtml/v2cshtml/FunctionCsvXmlBlobTrigger/FunctionCsvXmlBlobTrigger.cs
+++ 05-V2-Cshtml/v2cshtml/FunctionCsvXmlBlobTrigger/FunctionCsvXmlBlobTrigger.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Darren.Base;
+using Darren.Base.Model;
 using FunctionCsvXmlBlobTrigger.Services;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
@@ -13,18 +15,23 @@ namespace FunctionCsvXmlBlobTrigger
     public class FunctionCsvXmlBlobTrigger
     {
         [FunctionName("FunctionCsvXmlBlobTrigger")]
-        public async Task Run([BlobTrigger("international-transaction/{name}", Connection = "AzureWebJobsStorage")]Stream myBlob, string name, ILogger log)
+        public async Task Run([BlobTrigger("international-transaction/{blobName}.{blobExtension}", 
+                    Connection = "AzureWebJobsStorage")]Stream myBlob,
+                    string blobName,
+                    string blobExtension,
+                    string blobTrigger, // full path to triggering blob
+                    Uri uri, // blob primary location
+                    ILogger log)
+
         {
-            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
-            string ext = name.Split(".").Last();
-            if (name.Contains("good-file"))
+            if (blobTrigger.Contains("good-file"))
             {
-                ExpandLineItemCSV ecsv = new ExpandLineItemCSV();
-                CsvTransactionItemBase item = await ecsv.GetCsvRows(myBlob);
+                if (blobExtension.ToUpper() == ConstValues.CSV)
+                {
+                    ExpandLineItemCSV ecsv = new ExpandLineItemCSV();
+                    List<CsvTransactionItemBase> lsCsv = await ecsv.GetCsvRows(myBlob);
+                }
             }
-
-
-
         }
     }
 }
