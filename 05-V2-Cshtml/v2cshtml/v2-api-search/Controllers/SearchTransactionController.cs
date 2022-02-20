2a3f35bae51a3eb2b08cb2cb774a387925e1af19
darren <darren.ly.ong@kerrylogistics.com>
2022-02-20 09:54:47 +0800

    feat: able to get token using temp user

---
+++ 05-V2-Cshtml/v2cshtml/v2-api-search/Controllers/SearchTransactionController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace v2_api_search.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class SearchTransactionController : ControllerBase
+    {
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpGet("{currencycode}")]
+
+        public async Task<IActionResult> TransactionsByCurrency([FromRoute] string currencycode)
+        {
+            return Ok("a: " + currencycode);
+        }
+    }
+}
