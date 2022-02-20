d1170c71e997c2d831b0fffe9578ada52df68cfe
darren <darren.ly.ong@kerrylogistics.com>
2022-02-20 08:42:37 +0800

    feat: add v2-api-search project

---
+++ 05-V2-Cshtml/v2cshtml/v2-api-search/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace v2_api_search.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet("hello")]
+        public IActionResult hello()
+        {
+            return Ok("hello123");
+        }
+    }
+}
