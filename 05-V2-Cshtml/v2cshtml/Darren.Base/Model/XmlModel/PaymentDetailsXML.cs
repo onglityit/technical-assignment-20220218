5dd6d5d7c67537d2b37d1df8123d3a891f91f265
darren <darren.ly.ong@kerrylogistics.com>
2022-02-20 13:20:49 +0800

    feat: wip - xml model

---
+++ 05-V2-Cshtml/v2cshtml/Darren.Base/Model/XmlModel/PaymentDetailsXML.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Darren.Base.Model.XmlModel
+{
+    [XmlRoot(ElementName = "PaymentDetails")]
+    public class PaymentDetailsXML
+    {
+        [XmlElement(ElementName = "Amount")]
+        public string Amount { get; set; }
+        [XmlElement(ElementName = "CurrencyCode")]
+        public string CurrencyCode { get; set; }
+    }
+}
---
+++ 05-V2-Cshtml/v2cshtml/Darren.Base/Model/XmlModel/TransactionXML.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Darren.Base.Model.XmlModel
+{
+    [XmlRoot(ElementName = "Transaction")]
+    public class TransactionXML
+    {
+        [XmlAttribute(AttributeName = "Id")]
+        public string Id { get; set; }
+    }
+}
